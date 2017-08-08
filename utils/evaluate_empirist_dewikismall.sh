9759a0107f1068c5d02b2e161276c92e19d8931d
Thomas Proisl <thomas.proisl@fau.de>
2017-08-08 10:45:01 +0200

    Adjust filenames

--- utils/evaluate_empirist_dewikismall.sh
+++ utils/evaluate_empirist_dewikismall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 time ../bin/nlp4py-tagger --train tiger+dewikismall.model --brown /data/dewiki_brown.txt <(cut -f1,3 /cip/corpora/German/Tiger/Tiger2.1/corpus/tiger_release_aug07.txt)
-time ../bin/nlp4py-tagger --train tiger+dewikismall+empirist.model --prior tiger+dewikismall.model --brown /data/dewiki_brown.txt ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/train_all_sent.txt
-../bin/nlp4py-tagger --evaluate tiger+dewikismall+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_cmc_sent.txt
-../bin/nlp4py-tagger --evaluate tiger+dewikismall+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_web_sent.txt
+time ../bin/nlp4py-tagger --train tiger+dewikismall+empirist.model --prior tiger+dewikismall.model --brown /data/dewiki_brown.txt ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/train_all_sent_emo.txt
+../bin/nlp4py-tagger --evaluate tiger+dewikismall+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_cmc_sent_emo.txt
+../bin/nlp4py-tagger --evaluate tiger+dewikismall+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_web_sent_emo.txt
--- utils/evaluate_empirist_morphy.sh
+++ utils/evaluate_empirist_morphy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 time ../bin/nlp4py-tagger --train tiger+morphy.model --lexicon /cip/corpora/Lexicon/Morphy/morphy_wc.txt <(cut -f1,3 /cip/corpora/German/Tiger/Tiger2.1/corpus/tiger_release_aug07.txt)
-time ../bin/nlp4py-tagger --train tiger+morphy+empirist.model --prior tiger+morphy.model --lexicon /cip/corpora/Lexicon/Morphy/morphy_wc.txt ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/train_all_sent.txt
-../bin/nlp4py-tagger --evaluate tiger+morphy+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_cmc_sent.txt
-../bin/nlp4py-tagger --evaluate tiger+morphy+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_web_sent.txt
+time ../bin/nlp4py-tagger --train tiger+morphy+empirist.model --prior tiger+morphy.model --lexicon /cip/corpora/Lexicon/Morphy/morphy_wc.txt ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/train_all_sent_emo.txt
+../bin/nlp4py-tagger --evaluate tiger+morphy+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_cmc_sent_emo.txt
+../bin/nlp4py-tagger --evaluate tiger+morphy+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_web_sent_emo.txt
