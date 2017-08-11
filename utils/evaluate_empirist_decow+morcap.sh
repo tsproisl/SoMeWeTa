9a262fb354a39c88ed9f16c04ba8647da0777250
Thomas Proisl <thomas.proisl@fau.de>
2017-08-11 10:09:57 +0200

    Evaluation settings

--- utils/evaluate_empirist_decow+morcap.sh
+++ utils/evaluate_empirist_decow+morcap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-time ../bin/nlp4py-tagger --train tiger+decow+morcap.model --brown /data/decow14_brown.txt --lexicon morphy+capitalization.txt <(cut -f1,3 /cip/corpora/German/Tiger/Tiger2.1/corpus/tiger_release_aug07.txt)
-time ../bin/nlp4py-tagger --train tiger+decow+morcap+empirist.model --prior tiger+decow+morcap.model --brown /data/decow14_brown.txt --lexicon morphy+capitalization.txt ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/train_all_sent_emo.txt
+time ../bin/nlp4py-tagger --train tiger+decow+morcap.model --brown /data/decow14_brown.txt --lexicon morphy+decow14_capitalization.txt <(cut -f1,3 /cip/corpora/German/Tiger/Tiger2.1/corpus/tiger_release_aug07.txt)
+time ../bin/nlp4py-tagger --train tiger+decow+morcap+empirist.model --prior tiger+decow+morcap.model --brown /data/decow14_brown.txt --lexicon morphy+decow14_capitalization.txt ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/train_all_sent_emo.txt
 ../bin/nlp4py-tagger --evaluate tiger+decow+morcap+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_cmc_sent_emo.txt
 ../bin/nlp4py-tagger --evaluate tiger+decow+morcap+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_web_sent_emo.txt
