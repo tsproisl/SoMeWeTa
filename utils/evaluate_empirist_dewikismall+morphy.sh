5d5d84be74e9edad1bfdab62821003619bd44775
Thomas Proisl <thomas.proisl@fau.de>
2017-08-11 10:09:13 +0200

    Evaluation settings

--- utils/evaluate_empirist_dewikismall+morphy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-time ../bin/nlp4py-tagger --train tiger+dewikismall+morphy.model --brown /data/dewiki_brown.txt --lexicon /cip/corpora/Lexicon/Morphy/morphy_wc.txt <(cut -f1,3 /cip/corpora/German/Tiger/Tiger2.1/corpus/tiger_release_aug07.txt)
-time ../bin/nlp4py-tagger --train tiger+dewikismall+morphy+empirist.model --prior tiger+dewikismall+morphy.model --brown /data/dewiki_brown.txt --lexicon /cip/corpora/Lexicon/Morphy/morphy_wc.txt ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/train_all_sent_emo.txt
-../bin/nlp4py-tagger --evaluate tiger+dewikismall+morphy+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_cmc_sent_emo.txt
-../bin/nlp4py-tagger --evaluate tiger+dewikismall+morphy+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_web_sent_emo.txt
