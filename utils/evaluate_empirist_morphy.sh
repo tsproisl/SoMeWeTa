ec2dff437faae1db057e3f526f664b9e86e7d0d4
Thomas Proisl <thomas.proisl@fau.de>
2017-08-11 17:57:03 +0200

    Updated paths

--- utils/evaluate_empirist_morphy.sh
+++ utils/evaluate_empirist_morphy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-time ../bin/nlp4py-tagger --train tiger+morphy.model --lexicon /cip/corpora/Lexicon/Morphy/morphy_wc.txt <(cut -f1,3 /data/Thomas/tiger_release_aug07.txt)
-time ../bin/nlp4py-tagger --train tiger+morphy+empirist.model --prior tiger+morphy.model --lexicon /cip/corpora/Lexicon/Morphy/morphy_wc.txt ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/train_all_sent_emo.txt
+time ../bin/nlp4py-tagger --train tiger+morphy.model --lexicon /data/Thomas/morphy_wc.txt <(cut -f1,3 /data/Thomas/tiger_release_aug07.txt)
+time ../bin/nlp4py-tagger --train tiger+morphy+empirist.model --prior tiger+morphy.model --lexicon /data/Thomas/morphy_wc.txt ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/train_all_sent_emo.txt
 ../bin/nlp4py-tagger --evaluate tiger+morphy+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_cmc_sent_emo.txt
 ../bin/nlp4py-tagger --evaluate tiger+morphy+empirist.model ~/Documents/Arbeit/nlp4py/data/empirist_gold_standard/test_web_sent_emo.txt
