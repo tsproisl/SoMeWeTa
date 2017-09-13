0d1e3c57cbe694b54a28b72f242452032281cde0
Thomas Proisl <thomas.proisl@fau.de>
2017-09-13 10:29:21 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+# 1. Upload to PyPI:
+# python3 setup.py sdist
+# python3 setup.py sdist upload
+#
+# 2. Check if everything looks all right: https://pypi.python.org/pypi/SoMeWeTa
+#
+# 3. Go to https://github.com/tsproisl/SoMeWeTa/releases/new and
+# create a new release
+
+from os import path
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst')) as fh:
+    long_description = fh.read()
+
+version = "0.9.0"
+
+setup(
+    name='SoMeWeTa',
+    version=version,
+    author='Thomas Proisl',
+    author_email='thomas.proisl@fau.de',
+    packages=[
+        'someweta',
+    ],
+    scripts=[
+        'bin/somewe-tagger',
+    ],
+    url="https://github.com/tsproisl/SoMeWeTa",
+    download_url='https://github.com/tsproisl/SoMeWeTa/archive/v%s.tar.gz' % version,
+    license='GNU General Public License v3 or later (GPLv3+)',
+    description='A part-of-speech tagger for German web and social media texts.',
+    long_description=long_description,
+    install_requires=[
+        "numpy",
+    ],
+    python_requires='>=3.4',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: German',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+)
