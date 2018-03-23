360a39ea28283574411370d08f775869738c0e72
Thomas Proisl <thomas.proisl@fau.de>
2018-03-23 14:18:40 +0100

    Added XML support

--- someweta/utils.py
+++ someweta/utils.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 import collections
+import itertools
 import json
+import logging
 import math
+import re
+import warnings
+import xml.etree.ElementTree as ET
 
 
 def read_lexicon(filename):
@@ -118,3 +123,76 @@ def evaluate(gold, predicted, ignore_tag=None):
     if ignore_tag is not None:
         total = sum(1 for gt in gold if gt != ignore_tag)
     return correct / total
+
+
+def parse_xml(xml, is_file=True):
+    """Return a list of XML elements and their text/tail as well as the
+    whole text of the document.
+
+    """
+    Element = collections.namedtuple("Element", ["element", "type", "text"])
+
+    def text_getter(elem):
+        text = elem.text
+        tail = elem.tail
+        if text is None:
+            text = ""
+        if tail is None:
+            tail = ""
+        yield Element(elem, "text", text)
+        for child in elem:
+            for t in text_getter(child):
+                yield t
+        yield Element(elem, "tail", tail)
+    if is_file:
+        tree = ET.parse(xml)
+        root = tree.getroot()
+    else:
+        root = ET.fromstring(xml)
+    elements = list(text_getter(root))
+    return elements
+
+
+def read_xml(xml, tagged=True, is_file=True):
+    elements = parse_xml(xml, is_file)
+    whole_text = "".join((e.text for e in elements))
+    whole_text = re.sub(r"^\n+", "", whole_text)
+    whole_text = re.sub(r"\n+$", "\n", whole_text)
+    whole_text = re.sub(r"\n{3,}", "\n\n", whole_text)
+    return read_corpus(whole_text.split("\n"), tagged), elements
+
+
+def recreate_xml(tagged_tokens, elements):
+    """Introduce the tags from tagged_tokens into the elements and create
+    string with XML.
+
+    """
+    agenda = list(reversed(tagged_tokens))
+    for element in elements:
+        original_tokens = element.text.split("\n")
+        output = []
+        for ot in original_tokens:
+            if ot == "":
+                output.append(ot)
+            elif len(agenda) > 0 and agenda[-1][0] == ot:
+                tt = agenda.pop()
+                output.append("\t".join(tt))
+            else:
+                warnings.warn("Cannot match tagged tokens with XML input.")
+        if len(output) > 0:
+            tagged_text = "\n".join(output)
+        else:
+            tagged_text = "\n"
+            warnings.warn("Output should not be empty.")
+        if element.type == "text":
+            element.element.text = tagged_text
+        elif element.type == "tail":
+            element.element.tail = tagged_text
+    try:
+        assert len(agenda) == 0
+    except AssertionError:
+        warnings.warn("AssertionError: %d tokens left over" % len(agenda))
+    xml = ET.tostring(elements[0].element, encoding="unicode")
+    if xml[-1] != "\n":
+        xml += "\n"
+    return xml
