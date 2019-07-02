f28bf7cf158fcf35f19cdf5c3143e0905c5a3c05
Thomas Proisl <thomas.proisl@fau.de>
2019-07-02 13:40:14 +0200

    Unicode flags are single tokens

--- someweta/tagger.py
+++ someweta/tagger.py
@@ -66,7 +66,7 @@ class ASPTagger(AveragedStructuredPerceptron):
                                    r"|".join([re.escape(_) for _ in emoticon_list]) +
                                    r"$", re.VERBOSE)
         # Unicode emoticons and other symbols
-        self.unicode_flags = re.compile(r"\p{Regional_Indicator}{2}")
+        self.unicode_flags = re.compile(r"^\p{Regional_Indicator}{2}$")
         # self.emoji = re.compile(r"^[\u2600-\u27BF\uFE0E\uFE0F\U0001F300-\U0001f64f\U0001F680-\U0001F6FF\U0001F900-\U0001F9FF]$")
         self.emoji = re.compile(r"[\p{Extended_Pictographic}\p{Emoji_Presentation}\uFE0F]")
 
