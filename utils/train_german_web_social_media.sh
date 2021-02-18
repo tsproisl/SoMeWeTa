a631b05249a0c7234108689605f190305ab6f7a2
Thomas Proisl <thomas.proisl@fau.de>
2021-02-18 09:19:52 +0100

    Update training and evaluation scripts

--- utils/train_german_web_social_media.sh
+++ utils/train_german_web_social_media.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-../bin/somewe-tagger --train ../data/ignore/german_web_social_media_vX.Y.Z.model --prior ../data/ignore/german_newspaper_v1.0.0.model --brown /data/Thomas/decow14_brown.txt --lexicon /data/Thomas/morphy_wc.txt <(cat ../data/ignore/empirist_gold_standard/train_all_sent_emo.txt ../data/ignore/empirist_gold_standard/test_all_sent_emo.txt ../data/additional_training_german_web_social_media.txt)
+# prior
+../bin/somewe-tagger --train ../data/ignore/german_newspaper_for_empirist_yyyy-mm-dd.model \
+		     --brown /data/Thomas/decow14_brown.txt \
+		     --lexicon /data/Thomas/morphy_medium+contractions.txt \
+		     <(cut -f2,5 /data/Thomas/tiger_2.2_pav_instead_of_proav.conll09 \
+			         ../data/tiger_additional_quotes.conll09)
+
+# cmc model
+../bin/somewe-tagger --train ../data/ignore/german_web_social_media_yyyy-mm-dd.model \
+		     --prior ../data/ignore/german_newspaper_for_empirist_yyyy-mm-dd.model \
+		     --brown /data/Thomas/decow14_brown.txt \
+		     --lexicon /data/Thomas/morphy_medium+contractions.txt \
+		     <(cat ../data/ignore/empirist_train.txt \
+			   ../data/ignore/empirist_test.txt \
+			   ../data/additional_training_german_web_social_media.txt)
