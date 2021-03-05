7e8a92a745a43524a471d57ee72ae06c1b75391f
Thomas Proisl <thomas.proisl@fau.de>
2021-03-05 16:06:26 +0100

    Update build instructions

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # 1. Upload to PyPI:
-# python3 setup.py sdist
-# python3 setup.py sdist upload
+# (Note: Building 'bdist_wheel' requires package 'wheel')
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
 #
 # 2. Check if everything looks all right: https://pypi.python.org/pypi/SoMeWeTa
 #
